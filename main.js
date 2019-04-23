d865f09340f2ec06071daf5cd1dea4c1fcfd48cd
gatodex <boyzdex@gmail.com>
2019-04-23 12:06:03 -0400

    display information to html

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 
-  
-   var rut;
-
  function consulta() {
+ 	   var rut;
     var rut = document.getElementById("rut").value;
    
    jQuery.getJSON('https://siichile.herokuapp.com/consulta', {rut: rut}, function(result) {
-  console.log(result);})
 
-   
+    rut1 = JSON.stringify(result ,["rut"],'') ;
+    nom = JSON.stringify(result ,["razon_social"],'') ;
+    giro = JSON.stringify(result ,["actividades","giro",],'') ;
+    
+ //stripear nose hacerlo de otra maneroa asi que asi nomas
+    rut1 =  rut1.replace('{"rut":"' , "")
+    rut1 =  rut1.replace('"}' , "")
+
+    nom = nom.replace('{"razon_social":"',"")
+    nom = nom.replace('"}',"")
+
+    giro = giro.replace('{"actividades":[{"giro":"',"")
+    giro = giro.replace('(',"")
+    giro =giro.replace('"}]}',"")
+    giro = giro.replace('"}',"")
+
+    document.getElementById("1").innerHTML = rut1;
+    document.getElementById("2").innerHTML = nom;
+    document.getElementById("3").innerHTML = giro;
+
+
+   })
+
+
+//alert(JSON.stringify(result, null, 4));
+//document.getElementById("demo").innerHTML = 5 + 6;
+//console.log(result);
+//$('#stage').html('<p> Name: ' + jd.name + '</p>');
 }
 
- (function () {
-    var old = console.log;
-    var logger = document.getElementById('log');
-    console.log = function (message) {
-        if (typeof message == 'object') {
-            logger.textContent += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
-        } else {
-            logger.textContent += message + '<br />';
-        }
-    }
-})();
+
+
 
 	
